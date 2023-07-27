52d4496a2242b4697ee19c4ee6e8be06050b2f4a
choibohyun <etoile0320@naver.com>
2023-07-28 00:09:47 +0900

    project init_v1.0.1

---
+++ src/main/java/com/bhchoi/book_230727_v102/Book230727V102Application.java
@@ -0,0 +1,13 @@
+package com.bhchoi.book_230727_v102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Book230727V102Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Book230727V102Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bhchoi/book_230727_v102/Book230727V102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhchoi.book_230727_v102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Book230727V102ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
